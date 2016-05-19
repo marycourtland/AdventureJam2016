a46bdd94aed0fc2251bdb0c5fef8c9e1183d554c
Mary <marycbush@gmail.com>
2016-05-19 03:28:37 -0700

    Put the ui in its own module

--- prototype.js
+++ prototype.js
@@ -108,38 +108,5 @@ window.iterateMap = function() {
 }
 
 // UI/HUD
-
-window.UI = {};
-
-UI.infoTimeout = null;
-
-// Display info text for the specified lifetime
-// If lifetime isn't specified, then the text will stay up forever (until something else is shown)
-UI.info = function(text, lifetime) {
-
-    document.getElementById('info').textContent = text;
-
-    clearTimeout(UI.infoTimeout);
-    if (typeof lifetime === 'number') {
-        UI.infoTimeout = setTimeout(function() {
-            UI.info('', false);
-        }, lifetime)
-    }
-}
-
-// Display info text only while the given function is executing
-UI.infoWrap = function(text, fn) {
-    return function() {
-        UI.info(text);
-
-        setTimeout(function() {
-            fn();
-            UI.info('');
-        }, 0)
-    }
-}
-
-UI.zoomOut = UI.infoWrap('zooming...', function() { Map.zoomOut(); })
-UI.zoomIn = UI.infoWrap('zooming...', function() { Map.zoomIn(); })
-
+window.UI = require('./ui');
 window.onload = UI.infoWrap('loading...', initGame);
